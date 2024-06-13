2640a5753f84458711a07e151cba462bc4a73bb4
Molaei <molaie.web.developer@gmail.com>
2024-06-13 13:38:27 +0100

    Add spacing in beginPoster and chooseCoursePoster sections

--- src/components/content/poster/beginPoster.jsx
+++ src/components/content/poster/beginPoster.jsx
@@ -3,7 +3,7 @@ export const BeginPoster = () => {
     return (
         <div className="flex justify-center dark:bg-[#222222] bg-[#F5F5F5] px-4 py-2 lg:px-20 lg:py-12">
             <div className="w-full md:w-[80%] flex justify-between items-center gap-x-[20px]">
-                <div className="w-full sm:w-[50%] flex flex-wrap gap-y-2 xl:gap-y-3">
+                <div className="w-full sm:w-[50%] flex flex-wrap gap-y-8 sm:gap-y-4">
                     <a className="text-mainColor w-full" href="#">Let’s <span className="font-bold">Begins</span></a>
                     <h1 className="text-4xl lg:text-5xl dark:text-white xl:text-[70px] w-full font-[700]">Let's Find The Right <span className="text-[#545BE8]">Course</span> For you</h1>
                     <p className="text-grayText dark:text-white w-full text-xs lg:text-sm xl:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim, sem non convallis molestie.</p>
--- src/components/content/poster/chooseCoursePoster.jsx
+++ src/components/content/poster/chooseCoursePoster.jsx
@@ -10,16 +10,16 @@ export const SelectedCourseInfo = ({ number, name }) => {
 
 export const ChooseCoursePoster = () => {
     return (
-        <div className="bg-mainColor dark:bg-blue-950 w-full flex justify-center px-4 lg:px-20 sm:py-8">
-            <div className="flex justify-between gap-x-8 md:gap-x-0 w-full  md:w-[85%]">
-                <div className="hidden sm:block w-1/2 lg:w-[40%] h-[300px] md:h-[400px] lg:h-[500px]">
+        <div className="bg-mainColor dark:bg-blue-950 w-full flex justify-center items-center px-4 lg:px-20 lg:py-8">
+            <div className="flex justify-between items-center gap-x-8 md:gap-x-0 w-full  md:w-[85%]">
+                <div className="hidden sm:block w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] xl:w-[500px] xl:h-[500px]">
                     <img className="w-full h-full" src="./Group 1964.png" alt="Logo" />
                 </div>
                 <div className="w-full sm:w-[60%] md:w-[40%] lg:w-1/2 py-4 sm:py-10 lg:py-20">
                     <a className="w-full text-base sm:text-xs md:text-base text-[#F0C932]" href="#">SELECTED COURSE</a>
-                    <h1 className="w-full xl:w-1/2 font-[700] text-white text-4xl sm:text-2xl md:text-4xl mt-2 md:mt-4">People Taking Courses</h1>
-                    <p className="w-full xl:w-1/2 text-[#DDDDDD] text-sm sm:text-xs md:text-sm mt-2 md:mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim, sem non convallis molestie.</p>
-                    <ul className="w-full flex justify-center h-20 mt-2 md:mt-4">
+                    <h1 className="w-full xl:w-1/2 font-[700] text-white text-4xl sm:text-2xl md:text-4xl mt-4">People Taking Courses</h1>
+                    <p className="w-full xl:w-1/2 text-[#DDDDDD] text-sm sm:text-xs md:text-sm mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim, sem non convallis molestie.</p>
+                    <ul className="w-full flex justify-center h-20 mt-4">
                         <SelectedCourseInfo number="6,000" name="People Views" />
                         <SelectedCourseInfo number="4,000" name="User" />
                         <SelectedCourseInfo number="100" name="Course" />
