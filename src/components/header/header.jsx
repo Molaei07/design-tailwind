a1c168b8199d9ac71175e7b9ab94892f80dbb6eb
Molaei <molaie.web.developer@gmail.com>
2024-06-13 16:56:22 +0100

    change custom class and use tailwindcss class and remove "ItemsMenu" component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,41 +1,30 @@
-// eslint-disable-next-line react/prop-types
-export const ItemsMenu = ({ name }) => {
-    return (
-        <>
-            <li className="dark:text-white cursor-pointer hover:font-bold w-1/5 text-center transition-all duration-100">{name}</li>
-        </>
-    );
-}
-
 export const Header = () => {
     return (
-        <>
-            <header className="w-full dark:bg-black gap-y-4 px-[10px] sm:px-[50px] py-4 flex justify-between items-center flex-wrap shadow-lg lg:justify-evenly lg:gap-y-0">
-                {/* Logo */}
-                <div className="w-[40%] flex justify-start gap-x-2 items-center lg:w-[150px]">
-                    <div className="w-[30px] h-[30px]">
-                        <img src="./Logo.png" alt="Logo" className="w-full h-full" />
-                    </div>
-                    <span className="text-mainColor dark:text-white text-[12px]">mangcoding Store</span>
+        <header className="w-full dark:bg-black gap-y-4 px-2.5 sm:px-[50px] py-4 flex justify-between items-center flex-wrap shadow-lg lg:justify-evenly lg:gap-y-0">
+            {/* Logo */}
+            <div className="w-2/5 flex justify-start gap-x-2 items-center lg:w-[150px]">
+                <div className="w-[30px] h-[30px]">
+                    <img src="./Logo.png" alt="Logo" className="w-full h-full" />
                 </div>
-                {/* Item in menu */}
-                <ul className="w-full order-1 flex justify-between text-[12px] lg:w-[40%] lg:order-none sm:text-[15px]">
-                    <ItemsMenu name="Home" />
-                    <ItemsMenu name="Course" />
-                    <ItemsMenu name="About Us" />
-                    <ItemsMenu name="Article" />
-                    <ItemsMenu name="Contact" />
-                </ul>
-                {/* Btn to sing */}
-                <div className="w-[50%] flex justify-between gap-x-[5px] lg:w-[20%] lg:gap-x-[20px]">
-                    <div className="text-mainColor dark:text-white  border-2 border-mainColor border-solid entryBtn dark:hover:bg-blue-900/50">
-                        Log in
-                    </div>
-                    <div className="registerBtn">
-                        Register
-                    </div>
+                <span className="text-mainColor dark:text-white text-xs">mangcoding Store</span>
+            </div>
+            {/* Item in menu */}
+            <ul className="w-full order-1 flex justify-between text-xs lg:w-2/5 lg:order-none sm:text-sm">
+                <li className="itemMenu">Home</li>
+                <li className="itemMenu">Course</li>
+                <li className="itemMenu">About Us</li>
+                <li className="itemMenu">Article</li>
+                <li className="itemMenu">Contact</li>
+            </ul>
+            {/* Btn to sing */}
+            <div className="w-1/2 flex justify-between gap-x-1 lg:w-[20%] lg:gap-x-5">
+                <div className="text-mainColor dark:text-white  border-2 border-mainColor border-solid entryBtn dark:hover:bg-blue-900/50">
+                    Log in
                 </div>
-            </header>
-        </>
+                <div className="registerBtn">
+                    Register
+                </div>
+            </div>
+        </header>
     );
 }
