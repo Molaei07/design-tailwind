dbcedcc31197d32d7c813be993082181c514b0a9
Molaei <molaie.web.developer@gmail.com>
2024-06-16 13:45:35 +0100

    change way export

--- src/components/footer/createLinksFooter.jsx
+++ src/components/footer/createLinksFooter.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-export const LinksInFooter = ({partName, link1, link2, link3, link4}) => {
+const LinksInFooter = ({partName, link1, link2, link3, link4}) => {
     return (
         <li className="w-2/4 lg:w-1/4 h-fit flex flex-wrap gap-y-1 lg:gap-y-2">
-            <h1 className="w-full font-bold text-white text-center sm:text-left mb-2">{partName}</h1>
+            <h1 className="w-full font-bold text-white mb-2">{partName}</h1>
             <a href="#" className="linkInFooter">{link1}</a>
             <a href="#" className="linkInFooter">{link2}</a>
             <a href="#" className="linkInFooter">{link3}</a>
@@ -10,3 +10,5 @@ export const LinksInFooter = ({partName, link1, link2, link3, link4}) => {
         </li>
     );
 }
+
+export default LinksInFooter;
