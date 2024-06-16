8287ca0e21c79c8e7a34bc8a335af9b1b58f7365
Molaei <molaie.web.developer@gmail.com>
2024-06-16 13:45:01 +0100

    add localStorage

--- src/components/darkMode/darkMode.jsx
+++ src/components/darkMode/darkMode.jsx
@@ -1,13 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-export const DarkMode = ({darkMode, setDarkMode}) => {
+import { useEffect, useState } from "react";
+
+const DarkMode = () => {
+    const [darkMode, setDarkMode] = useState()
+
+    useEffect(() => {
+        let theme = localStorage.getItem('theme')
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark', 'bg-darkGray')
+            setDarkMode('dark')
+        }
+        if (theme === 'light') {
+            setDarkMode('light')
+        }
+    }, [])
 
     const toggleDarkMode = () => {
-        setDarkMode(!darkMode)
+        if (darkMode === 'dark') {
+            document.documentElement.classList.remove('dark', 'bg-darkGray')
+            setDarkMode('light')
+            localStorage.setItem('theme', 'light')
+        } else {
+            document.documentElement.classList.add('dark', 'bg-darkGray')
+            setDarkMode('dark')
+            localStorage.setItem('theme', 'dark')
+        }
     }
 
     return (
-        <button onClick={toggleDarkMode} className='font-mono p-4 font-bold text-white bg-slate-900 dark:bg-white dark:text-slate-900 rounded-full fixed left-8 bottom-20'>
-            {darkMode ? 'LIT' : 'DRK'}
-        </button>
+        <>
+            <button onClick={toggleDarkMode} className='font-mono p-4 font-bold text-white bg-slate-900 dark:bg-white dark:text-slate-900 rounded-full fixed left-8 bottom-20'>
+                {darkMode === 'dark' ? 'LIT' : 'DRK'}
+            </button>
+        </>
     );
 }
+
+export default DarkMode;
